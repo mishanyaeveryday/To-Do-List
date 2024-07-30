373a11a926b55a5ea029645a265f8134d77eed2f
mishakostenko <mishakostenko70@gmail.com>
2024-07-30 16:50:30 +0200

    fix: fixed struct Task

--- Task.cpp
+++ Task.cpp
@@ -1,11 +1,10 @@
 #include "Task.h"
 
-Task::Task(int id, const std::string& title, const std::string &description) : 
-          id(id), title(title), description(description), completed_task(false){}
-
-          std::ostream& operator<<(std::ostream &os, const Task &task){
-            if(task.completed_task)
-            return os << task.id << " [" << task.title << ": " << task.description << "] " << "- completed"; 
-              else 
-                return os << task.id << " [" << task.title << ": " << task.description << "] " << "- not completed"; 
-          }
+// Реализация оператора вывода
+std::ostream& operator<<(std::ostream &os, const Task &task) {
+    if (task.completed) {
+        return os << task.id << " [" << task.title << ": " << task.description << "] - completed";
+    } else {
+        return os << task.id << " [" << task.title << ": " << task.description << "] - not completed";
+    }
+}
--- Task.h
+++ Task.h
@@ -1,15 +1,20 @@
 #ifndef TASK_H
 #define TASK_H
-#include <ostream>
+
+#include <iostream>
 #include <string>
 
-struct Task{
+struct Task {
     int id;
     std::string title;
     std::string description;
-    bool completed_task;
-    Task(int id, const std::string& title, const std::string &description);
+    bool completed;
+
+    Task(int id, const std::string& title, const std::string& description, bool completed)
+        : id(id), title(title), description(description), completed(completed) {}
+
+    // Перегрузка оператора вывода должна быть объявлена как дружественная функция
     friend std::ostream& operator<<(std::ostream &os, const Task &task);
 };
 
-#endif
+#endif // TASK_H
